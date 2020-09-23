5b23f1f7f1d78589d93e26de77466ca08fa40c3d
sumin <sumiiniii@ajou.ac.kr>
2020-09-23 19:41:22 +0900

    footer 기능

---
+++ footer.js
@@ -0,0 +1,28 @@
+const footerContainer = document.querySelector(".js-footer");
+
+function getFaceBookLink(){
+    document.querySelector('#facebook_btn').addEventListener('click',function(){
+        location.href="https://www.facebook.com/AjouHantor/";
+    })
+    
+}
+function getInstagramkLink(){
+    document.querySelector('#instagram_btn').addEventListener('click',function(){
+        location.href="https://www.instagram.com/_suminvely/";
+    })
+    
+}
+function getKakaoLink(){
+    document.querySelector('#kakaotalk_btn').addEventListener('click',function(){
+        location.href="https://open.kakao.com/o/sdJ9xmyc";
+    })
+    
+}
+
+function init(){
+    getFaceBookLink();
+    getInstagramkLink();
+    getKakaoLink();
+}
+
+init();
