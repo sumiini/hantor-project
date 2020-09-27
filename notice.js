c1c27d0cc5c2f163d21e2d66865bc427d8de4c75
sumin <sumiiniii@ajou.ac.kr>
2020-09-27 15:37:14 +0900

    localstorage 저장 (테이블뷰생성,css필요)

--- notice.js
+++ notice.js
@@ -3,26 +3,46 @@ let noticeUl = document.getElementById("noticeCo");
 let cnt = 0;
 let today = new Date();
 
+let year = today.getFullYear(); // 년도
+let month = today.getMonth() + 1;  // 월
+let date = today.getDate();  // 날짜
 
 //forEach 랑 filter는 list에 있는 모든 item을 위한 함수를 실행시키는 것
+let addNotice = document.createElement("li");
+let newNotice = document.querySelector("textarea");
+
+let noticeContentArr = [];
+function saveContent(){
+    localStorage.setItem('noticeContent',JSON.stringify(noticeContentArr));
+
+
+}
 
 noticeBtn.addEventListener("click" ,function(){
     cnt++;
 
-    let addNotice = document.createElement("li");
-    let newNotice = document.querySelector("input");
+    
+    let todayDate = '\t'+year+'/'+month+'/'+date;
 
-    addNotice.innerHTML = document.getElementById("inputID").value;
+    addNotice.innerHTML = newNotice.value +'\t'+todayDate;
 
 
     
     noticeUl.appendChild(addNotice);
+
+    const noticeObj = {
+        content: newNotice.value,
+        todayDay : todayDate
+    };
+    noticeContentArr.push(noticeObj);
+
+    
+
+    saveContent();
+
     
     
-
-
-
-    
-
 })
+
+
 
