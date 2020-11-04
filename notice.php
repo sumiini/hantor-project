3cf1f6014498f5d477cc981c41a569dbc568e12e
sumin <sumiiniii@ajou.ac.kr>
2020-11-04 22:12:49 +0900

    db수정

--- notice.php
+++ notice.php
@@ -14,13 +14,13 @@ $mysqli = new mysqli($host, $user, $pw, $dbName);
  $content=$_POST['content'];
  
  
- $sql = "insert into notice (number, date, content)";             // (입력받음)insert into 테이블명 (column-list)
- $sql = $sql. "values('$number','$date','$content')";         // calues(column-list에 넣을 value-list)
+ $sql = "insert into notice (content)";             // (입력받음)insert into 테이블명 (column-list)
+ $sql = $sql. "values('$content')";         // calues(column-list에 넣을 value-list)
  if($mysqli->query($sql)){                                                              //만약 sql로 잘 들어갔으면
   echo 'success inserting <p/>';                                                            //success inserting 으로 표시
-  echo $date.'에 공지사항이 추가 되었습니다.<p/>';                                   // id님 안녕하세요.
+  echo $date.'에 공지사항이 추가 되었습니다.<p/>';                                   // success
  }else{                                                                                //아니면
-  echo 'fail to insert sql';                                                            //fail to insert sql로 표시
+  echo 'fail to insert sql';                                                            //fail to insert sql로
  }
 mysqli_close($mysqli);
  
