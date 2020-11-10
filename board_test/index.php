f3e48d3890bd3b1e552c3b839fda4ae2432a7b24
dondon17 <qwerqw889@ajou.ac.kr>
2020-11-10 17:14:18 +0900

    print db content

--- board_test/index.php
+++ board_test/index.php
@@ -1,38 +1,39 @@
-<?php include  $_SERVER['DOCUMENT_ROOT']."/db.php"; ?>
-<!doctype html>
+<!-- <!doctype html>
 <head>
 <meta charset="UTF-8">
 <title>게시판</title>
 <link rel="stylesheet" type="text/css" href="/css/style.css" />
 </head>
 <body>
+<?php
+  // echo "hello";
+
+  
+  // $connect = mysql_connect("localhost", "dondon", "5Susdpdlf!");
+  // $db_con = mysql_select_db("post_board", $connect);
+
+  // $sql = "select * from notice"
+  // $result = mysql_query($sql, $connect);
+
+  // $fields = mysql_num_fields($result);
+  // $number = 1;
+  echo phpinfo();
+?>
 <div id="board_area"> 
   <h1>자유게시판</h1>
   <h4>자유롭게 글을 쓸 수 있는 게시판입니다.</h4>
     <table class="list-table">
       <thead>
-          <tr>
-              <th width="70">번호</th>
-                <th width="500">제목</th>
-                <th width="120">글쓴이</th>
-                <th width="100">작성일</th>
-                <th width="100">조회수</th>
-            </tr>
-        </thead>
-        <?php
-        // board테이블에서 idx를 기준으로 내림차순해서 5개까지 표시
-          $sql = mq("select * from board order by idx desc limit 0,5"); 
-            while($board = $sql->fetch_array())
-            {
-              //title변수에 DB에서 가져온 title을 선택
-              $title=$board["title"]; 
-              if(strlen($title)>30)
-              { 
-                //title이 30을 넘어서면 ...표시
-                $title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
-              }
-        ?>
+        <tr>
+          <th width="70">번호</th>
+          <th width="500">제목</th>
+          <th width="120">글쓴이</th>
+          <th width="100">작성일</th>
+          <th width="100">조회수</th>
+        </tr>
+      </thead>
       <tbody>
+        
         <tr>
           <td width="70"><?php echo $board['idx']; ?></td>
           <td width="500"><a href=""><?php echo $title;?></a></td>
@@ -41,11 +42,10 @@
           <td width="100"><?php echo $board['hit']; ?></td>
         </tr>
       </tbody>
-      <?php } ?>
     </table>
     <div id="write_btn">
       <a href="/page/board/write.php"><button>글쓰기</button></a>
     </div>
   </div>
 </body>
-</html>
+</html> -->
--- notice.php
+++ notice.php
@@ -1,30 +1,29 @@
 <html>
    <meta charset="utf-8">
+   <?php
+   
+      $host = 'localhost';
+      $user = 'dondon';
+      $pw = '5Susdpdlf!';
+      $dbName = 'opentutorials';
+      $mysqli = new mysqli($host, $user, $pw, $dbName);
 
-<?php
- 
-$host = 'localhost';
-$user = 'dondon';
-$pw = '5Susdpdlf!';
-$dbName = 'opentutorials';
-$mysqli = new mysqli($host, $user, $pw, $dbName);
+      $number=$_POST['number'];
+      $date=$_POST['date'];
+      $content=$_POST['content'];
+      
+      
+      $sql = "insert into notice (content)";             // (입력받음)insert into 테이블명 (column-list)
+      $sql = $sql."values('$content')";         // calues(column-list에 넣을 value-list)
+      if($mysqli->query($sql)){                 //만약 sql로 잘 들어갔으면
+         echo 'success inserting.';                          //success inserting 으로 표시
+         echo $date."에 공지사항이 추가 되었습니다.<p/>";      // success
+      }
+      else{//아니면
+         echo 'fail to insert sql';//fail to insert sql로
+      }
+      mysqli_close($mysqli);
 
- $number=$_POST['number'];
- $date=($_POST['date']);
- $content=$_POST['content'];
- 
- 
- $sql = "insert into notice (content)";             // (입력받음)insert into 테이블명 (column-list)
- $sql = $sql. "values('$content')";         // calues(column-list에 넣을 value-list)
- if($mysqli->query($sql)){                                                              //만약 sql로 잘 들어갔으면
-  echo 'success inserting <p/>';                                                            //success inserting 으로 표시
-  echo $date.'에 공지사항이 추가 되었습니다.<p/>';                                   // success
- }else{                                                                                //아니면
-  echo 'fail to insert sql';                                                            //fail to insert sql로
- }
-mysqli_close($mysqli);
- 
- 
-?>
-<input type="button" value="다시 글 등록하기" onclick="location='notice.html'">
+   ?>
+   <input type="button" value="다시 글 등록하기" onclick="location='notice.html'">
 </html>
